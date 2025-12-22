0c1d87ae4911ca8a455131990ea60d2b363830e1
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-22 13:36:59 +0100

    add serving static files in Express server

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 
 export default app;
