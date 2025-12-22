cbcd951d77fa1d889baa7d255593865bcc8d4327
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-22 14:03:13 +0100

    fix missing .js imports in app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@
  * PURPOSE: The main "Rulebook" for the Express application.
  */
 import express from "express";
-import bookingsRouter from "./routes/bookings.routes";
-import roomsRouter from "./routes/rooms.routes";
-import authRouter from "./routes/auth.routes";
+import bookingsRouter from "./routes/bookings.routes.js";
+import roomsRouter from "./routes/rooms.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
