96bc762e3a96c1a15ff38f547c64639632afe445
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-22 13:39:26 +0100

    fix: filepath for static serving in express server

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("src/public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
